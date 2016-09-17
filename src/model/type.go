b5f929c481ce2785b51c00e2c7b6debce6f94307
mikanfactory <k952i4j14x17@gmail.com>
2016-09-17 10:59:25 +0900

    [add] implement type used for ajax request

--- src/model/type.go
+++ src/model/type.go
@@ -24,3 +24,18 @@ type Position struct {
 	Latitude    float64 `json:"lat"`
 	Longutitude float64 `json:"lng"`
 }
+
+// Setting denotes the setting of the list of marker.
+type Setting struct {
+	Target     string      `json:"target"`
+	Title      string      `json:"title"`
+	Conditions []Condition `json:"conditions"`
+}
+
+// Condition denotes how to deal the status.
+type Condition struct {
+	Feature  string  `json:"feature"`
+	Operator string  `json:"operator"`
+	Value    float64 `json:"value"`
+	Status   string  `json:"status"`
+}
