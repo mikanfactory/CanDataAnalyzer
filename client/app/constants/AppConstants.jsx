3d0ee3abf969c651b9d45c47aee14711bb00f4aa
mikanfactory <k952i4j14x17@gmail.com>
2016-09-21 16:05:18 +0900

    [WIP] rename constants

---
+++ client/app/constants/AppConstants.jsx
@@ -0,0 +1,19 @@
+import keyMirror from 'keymirror'
+
+export default {
+
+  ActionTypes: keyMirror({
+    UPDATE_GOOGLE_MAP: null,
+    ADD_NEW_SETTING: null,
+    ADD_NEW_CONDITION: null,
+    ADD_NEW_MARKERS: null,
+    DRAW_MARKERS: null,
+    ERASE_MARKERS: null,
+    DRAW_MARKER: null,
+    ERASE_MARKER: null,
+    OPEN_MODAL: null,
+    CLOSE_MODAL: null,
+    NEW_MODAL: null
+  })
+
+}
