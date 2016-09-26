46fb1242fc8ea7c900b783ce77ba28230f471cd0
mikanfactory <k952i4j14x17@gmail.com>
2016-09-26 22:38:08 +0900

    [WIP] impl grid layer handler

--- client/app/components/ToolBoxHeader.jsx
+++ client/app/components/ToolBoxHeader.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import ModalAction from '../actions/ModalActions'
+import LayerAction from '../actions/LayerActions'
+import { defaultDivideSize } from '../constants/AppConstants'
+import { createGridPoints } from '../utils/AppGoogleMapUtil'
 import { ToolBoxHeaderStyle as s } from './Styles'
 
 export default class ToolBoxHeader extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      isGridLayerVisible: false
+    }
+
+    this.handleGridLayerDisplay = this.handleGridLayerDisplay.bind(this)
   }
 
   handleModalOpen() {
     ModalAction.createModal()
+  }
+
+  handleGridLayerToggle() {
+    this.state.isGridLayerVisible?
+    this.handleGridLayerDisplay() :
+    this.handleGridLayerErase()
+
+    this.setState({ isGridLayerVisible: !this.isGridLayerVisible })
+  }
+
+  handleGridLayerDisplay() {
+    const gridPoints = createGridPoints(this.props.gMap, defaultDivideSize)
+    LayerAction.createGridPoints(gridPoints)
+  }
+
+  handleGridLayerErase() {
+    LayerAction.destroyGridPoints()
   }
 
   render() {
@@ -16,6 +42,10 @@ export default class ToolBoxHeader extends React.Component {
       <div>
         <div className="ToolBoxHeader" style={s.HeaderStyle}>
           <span style={s.StringStyle}>CanDataAnalyzer</span>
+          <span className="glyphicon glyphicon-th"
+                style={s.GlyphiconStyle}
+                onClick={this.handleGridLayerToggle}>
+          </span>
           <span className="glyphicon glyphicon-plus"
                 style={s.GlyphiconStyle}
                 onClick={this.handleModalOpen}>
@@ -28,4 +58,8 @@ export default class ToolBoxHeader extends React.Component {
       </div>
     )
   }
+}
+
+ToolBoxHeader.propTypes = {
+  gMap: React.PropTypes.object
 }
