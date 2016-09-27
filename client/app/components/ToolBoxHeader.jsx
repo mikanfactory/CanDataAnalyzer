cb561a319695e9ef3f97d6a9336ca11099f75fce
mikanfactory <k952i4j14x17@gmail.com>
2016-09-27 15:20:21 +0900

    [WIP] experiment heatmap

--- client/app/components/ToolBoxHeader.jsx
+++ client/app/components/ToolBoxHeader.jsx
@@ -4,6 +4,25 @@ import LayerAction from '../actions/LayerActions'
 import { getSmallerBounds } from '../utils/AppGoogleMapUtil'
 import { ToolBoxHeaderStyle as s } from './Styles'
 
+function getPoints() {
+  return _getPoints().map( px => {
+    return new window.google.maps.LatLng(...px)
+  })
+}
+
+function _getPoints() {
+  return [...Array(100).keys()].map( () => {
+    return [
+      36.08 + 0.01*getRandomArbitary(-1, 1),
+      140.21 + 0.01*getRandomArbitary(-1, 1)
+    ]
+  })
+}
+
+function getRandomArbitary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 export default class ToolBoxHeader extends React.Component {
   constructor(props) {
     super(props)
@@ -13,6 +32,7 @@ export default class ToolBoxHeader extends React.Component {
     }
 
     this.handleLayerToggle = this.handleLayerToggle.bind(this)
+    this.handleHeatmapToggle = this.handleHeatmapToggle.bind(this)
   }
 
   handleModalOpen() {
@@ -36,18 +56,30 @@ export default class ToolBoxHeader extends React.Component {
     LayerAction.destroyAllLayer()
   }
 
+  handleHeatmapToggle() {
+    const heatmap = new window.google.maps.visualization.HeatmapLayer({
+      data: getPoints(),
+      map: this.props.gMap,
+      radius: 100,
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="ToolBoxHeader" style={s.HeaderStyle}>
           <span style={s.StringStyle}>CanDataAnalyzer</span>
+          <span className="glyphicon glyphicon-plus"
+                style={s.GlyphiconStyle}
+                onClick={this.handleModalOpen}>
+          </span>
           <span className="glyphicon glyphicon-th"
                 style={s.GlyphiconStyle}
                 onClick={this.handleLayerToggle}>
           </span>
-          <span className="glyphicon glyphicon-plus"
+          <span className="glyphicon glyphicon-fire"
                 style={s.GlyphiconStyle}
-                onClick={this.handleModalOpen}>
+                onClick={this.handleHeatmapToggle}>
           </span>
           <span className="glyphicon glyphicon-film"
                 style={s.GlyphiconStyle}
