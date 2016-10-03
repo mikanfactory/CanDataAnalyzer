93577835b0399f616d40cd398b2e17e2cd42f260
mikanfactory <k952i4j14x17@gmail.com>
2016-10-03 14:20:32 +0900

    [add] show message if sending setting will fail

--- client/app/components/Message.jsx
+++ client/app/components/Message.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import Rodal from 'rodal'
 import MessageActions from '../actions/MessageActions'
-import MessageStore from '../stores/MessageStores'
+import MessageStore from '../stores/MessageStore'
 import { MessageStyle as s} from './Styles'
 import isEmpty from 'lodash/isEmpty'
 
@@ -11,10 +12,14 @@ function getStateFromStores() {
 }
 
 export default class Message extends React.Component {
-  construcor(props) {
+  constructor(props) {
     super(props)
 
     this.state = getStateFromStores()
+  }
+
+  handleMessageDelete() {
+    MessageActions.deleteMessage()
   }
 
   componentDidMount() {
@@ -26,9 +31,12 @@ export default class Message extends React.Component {
   }
 
   render() {
-    return isEmpty(this.state.message) ?
-           false :
-           <div>{this.state.message.text}</div>
+    return (
+      <Rodal visible={isEmpty(this.state.message)} width={400} height={720}
+             onClose={this.handleMessageDelete}>
+        <div className="Message" style={s.MessageStyle}>{this.state.message.text}</div>
+      </Rodal>
+    )
   }
 
   _onChange() {
