c09f32cd450952ed13e4eae3f3c98f79be9c133d
mikanfactory <k952i4j14x17@gmail.com>
2016-10-05 17:39:19 +0900

    [fix] don't show error if succeess in send heatmap

--- client/app/utils/AppWebAPIUtils.jsx
+++ client/app/utils/AppWebAPIUtils.jsx
@@ -1,9 +1,18 @@
 import MessageActions from '../actions/MessageActions'
 
-function checkStatus(json) {
+// TODO: integrate those functions
+function checkStatus1(json) {
   // if status == 200 then recieved json has no status field
   if (!json.code) {
     return json
+  }
+  const error = new Error(json.content)
+  throw error
+}
+
+function checkStatus2(json) {
+  if (json.code && json.code == 200) {
+    return json.content
   }
   const error = new Error(json.content)
   throw error
@@ -20,7 +29,7 @@ export function fetchMarkers(data, callback) {
     body: JSON.stringify(data)
   })
   .then(resp => resp.json())
-  .then(json => checkStatus(json))
+  .then(json => checkStatus1(json))
   .then(markers => callback(markers))
   .catch(err => MessageActions.createMessage({ text: err }))
 }
@@ -36,7 +45,7 @@ export function sendHeatmapSetting(data) {
     body: JSON.stringify(data)
   })
     .then(resp => resp.json())
-    .then(json => checkStatus(json))
+    .then(json => checkStatus2(json))
     .then(message => MessageActions.createMessage({ text: message }))
     .catch(err => MessageActions.createMessage({ text: err }))
 }
