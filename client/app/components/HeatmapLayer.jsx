eee1f1ab1b394ef9d6f4aaecb45632ea82c991ac
mikanfactory <k952i4j14x17@gmail.com>
2016-10-25 12:42:27 +0900

    [add] move heatmap layer to new file

---
+++ client/app/components/HeatmapLayer.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import LayerStore from '../stores/LayerStore'
+import MarkerStore from '../stores/MarkerStore'
+import { createGridPoints } from '../utils/AppGoogleMapUtil'
+import { convertMarkersToWeightedLocations } from '../utils/AppAlgorithmUtil'
+import { defaultDivideSize } from '../constants/AppConstants'
+
+import assign from 'object-assign'
+import isEqual from 'lodash/isEqual'
+import isEmpty from 'lodash/isEmpty'
+
+function getStateFromStores() {
+  return {
+    bounds: LayerStore.getBounds(),
+    isHeatmapVisible: LayerStore.getHeatmapVisibility(),
+  }
+}
+
+export default class HeatmapLayer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    const s = {
+      visibleHeatmap: {},
+    }
+    this.state = assign({}, getStateFromStores(), s)
+
+    this.drawHeatmap = this.drawHeatmap.bind(this)
+    this.eraseHeatmap = this.eraseHeatmap.bind(this)
+    this._onChange = this._onChange.bind(this)
+  }
+
+  drawHeatmap() {
+    const { bounds } = this.state
+    const gridPoints = createGridPoints(bounds, defaultDivideSize)
+    const markers = MarkerStore.getAllMarkers()
+    const wls = convertMarkersToWeightedLocations(markers, gridPoints)
+    const radius = zoomToRadius(this.props.gMap.getZoom())
+    const heatmap = new window.google.maps.visualization.HeatmapLayer({
+      data: wls,
+      map: this.props.gMap,
+      radius: radius
+    })
+
+    this.props.gMap.addListener('bounds_changed', () => {
+      const radius = zoomToRadius(this.props.gMap.getZoom())
+      this.state.visializeHeatmap.setOptions({ radius: radius })
+    })
+
+    this.setState({ visibleHeatmap: heatmap })
+  }
+
+  eraseHeatmap() {
+    this.state.visibleHeatmap.setMap(null)
+    this.setState({ visibleHeatmap: {} })
+  }
+
+  componentDidMount() {
+    LayerStore.addChangeListener(this._onChange)
+  }
+
+  componentWillUnmount() {
+    LayerStore.removeChangeListener(this._onChange)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // draw/erase heatmap
+    if (!isEqual(this.state.isHeatmapVisible, prevState.isHeatmapVisible)) {
+      isEmpty(this.state.visibleHeatmap) ?
+      this.drawHeatmap() :
+      this.eraseHeatmap()
+    }
+  }
+
+  render() {
+    return (
+      <div className="Layer" style={{height: 0, width: 0}}>
+        <div id="dummyHeatmap"></div>
+      </div>
+    )
+  }
+
+  _onChange() {
+    this.setState(getStateFromStores())
+  }
+}
+
+Layer.propTypes = {
+  gMap: React.PropTypes.object
+}
