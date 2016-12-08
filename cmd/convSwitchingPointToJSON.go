e367a85888c839c7754fb171ef7ac4910fae6df8
mikanfactory <k952i4j14x17@gmail.com>
2016-12-08 18:22:35 +0900

    [fix] fix bugs

--- cmd/convSwitchingPointToJSON.go
+++ cmd/convSwitchingPointToJSON.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 
@@ -28,12 +29,18 @@ func ConvSwitchingPointToJSON() {
 	}
 
 	validColumns := getValidColumns(cacheInfo)
+
+	markers := []model.Marker{}
 	for _, target := range targets.Names {
-		writeSwitchingPoint(db, target, validColumns)
+		ms := writeSwitchingPoint(db, target, validColumns)
+		markers = append(markers, ms...)
 	}
+
+	json, _ := json.Marshal(markers)
+	ioutil.WriteFile("data/middle/switch.json", json, 0744)
 }
 
-func writeSwitchingPoint(db *sql.DB, target string, validColumns []Column) {
+func writeSwitchingPoint(db *sql.DB, target string, validColumns []Column) []model.Marker {
 	cs, _ := getSwitchingPoint(db, target)
 
 	markers := []model.Marker{}
@@ -50,8 +57,7 @@ func writeSwitchingPoint(db *sql.DB, target string, validColumns []Column) {
 		}
 	}
 
-	json, _ := json.Marshal(markers)
-	ioutil.WriteFile("data/middle/switch.json", json, 0744)
+	return markers
 }
 
 func getStatus(prev, next model.Can) string {
@@ -70,7 +76,7 @@ func getStatus(prev, next model.Can) string {
 }
 
 func getSwitchingPoint(db *sql.DB, target string) ([]model.Can, error) {
-	query := "select * from cans"
+	query := fmt.Sprintf("select * from cans where target = '%s'", target)
 	rows, err := db.Query(query)
 	if err != nil {
 		return nil, err
