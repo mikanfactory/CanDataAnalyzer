c5bde1913d56865918d19ceaa31f9697841e870c
mikanfactory <k952i4j14x17@gmail.com>
2017-01-12 19:10:38 +0900

    [update] correspand to LaneCount

--- cmd/createConfig.go
+++ cmd/createConfig.go
@@ -44,14 +44,38 @@ func extractHeader(target string) CacheInfo {
 		"LAT_GPS[deg]":           "Latitude",
 		"BRKSWTM":                "Brake",
 		"RawPedal_APOFS[%]":      "Accel",
+		"TACHO[Tr/min]":          "Engine",
 		"Z_ABST_toAT[m]":         "AheadDistance",
-		"STRANGLE[deg]":          "SteeringAngle",
 		"Curve_0[\ufffd\ufffdm]": "CurveRadius",
+		"STRANGLE[deg]":          "SteeringAngle",
+		"D_BRANCH_FLG":           "BranchFlag",
 		"DistTollgate[m]":        "DistTollgate",
-		"DistDivergence[m]":      "DistDivergence",
 		"RoadType":               "RoadType",
+		"VC_GreenLamp":           "GreenLamp",
+		"VC_RedLamp":             "RedLamp",
+		"VC_RightLamp":           "RightLamp",
+		"VC_UpLamp":              "UpLamp",
+		"VC_LeftLamp":            "LeftLamp",
+		"VC_StopSign":            "StopSign",
+		"VC_30Sign":              "Limit30Sign",
+		"VC_50Sign":              "Limit50Sign",
+		"VC_CarBrake":            "BrakeCar",
+		"VC_LeftCar":             "LeftCar",
+		"VC_ManCycleCount":       "ManBicycleCount",
+		"VC_ManCycle":            "ManBicycle",
+		"VC_ManCycleDistance[m]": "DistManBicycle",
+		"VC_Pitch":               "Pitch",
+		"VC_Signal[m]":           "DistSignal",
+		"VC_NaviCls":             "PathType",
+		"VC_LaneCount":           "LaneCount",
+		"a":                      "AccelerationSpeed",
+		"Jerk":                   "Jerk",
+		"THW":                    "TimeHeadway",
+		"TTC":                    "TimeToCollision",
+		"RF":                     "RiskFactor",
 	}
 
+	LCcounter := 0
 	columns := []Column{}
 	for i, name := range header {
 		t, n, r := "float64", name, false
@@ -62,6 +86,13 @@ func extractHeader(target string) CacheInfo {
 
 		if convName[name] != "" {
 			n, r = convName[name], true
+		}
+
+		if convName[name] == "LaneCount" {
+			if LCcounter == 0 {
+				LCcounter++
+				continue
+			}
 		}
 
 		column := Column{
--- src/model/table.go
+++ src/model/table.go
@@ -9,10 +9,33 @@ type Can struct {
   Speed float64
   Brake int64
   Accel int64
+  Engine float64
   AheadDistance float64
   CurveRadius float64
   SteeringAngle float64
+  BranchFlag float64
   DistTollgate float64
-  DistDivergence float64
   RoadType float64
+  GreenLamp float64
+  RedLamp float64
+  RightLamp float64
+  UpLamp float64
+  LeftLamp float64
+  StopSign float64
+  Limit30Sign float64
+  Limit50Sign float64
+  BrakeCar float64
+  LeftCar float64
+  ManBicycleCount float64
+  ManBicycle float64
+  DistManBicycle float64
+  Pitch float64
+  DistSignal float64
+  PathType float64
+  LaneCount float64
+  AccelerationSpeed float64
+  Jerk float64
+  TimeHeadway float64
+  TimeToCollision float64
+  RiskFactor float64
 }
