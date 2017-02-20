ef946900238e305818052addd03242d87db677ac
mikanfactory <k952i4j14x17@gmail.com>
2017-02-20 16:25:03 +0900

    [update] configs

--- src/model/table.go
+++ src/model/table.go
@@ -3,11 +3,35 @@ package model
 type Can struct {
   ID int64
   Target string
-  Latitude float64
-  Longitude float64
-  GPSSpeed float64
   Time float64
-  Brake float64
-  Accel float64
+  Longitude float64
+  Latitude float64
+  Speed float64
+  Brake int64
+  Accel int64
+  Engine float64
+  AheadDistance float64
   SteeringAngle float64
+  BranchFlag float64
+  DistTollgate float64
+  RoadType float64
+  GreenLamp float64
+  RedLamp float64
+  RightLamp float64
+  UpLamp float64
+  LeftLamp float64
+  StopSign float64
+  Limit30Sign float64
+  Limit50Sign float64
+  BrakeCar float64
+  LeftCar float64
+  ManBicycleCount float64
+  ManBicycle float64
+  DistManBicycle float64
+  Pitch float64
+  DistSignal float64
+  PathType float64
+  LaneCount float64
+  AccelerationSpeed float64
+  Jerk float64
 }
