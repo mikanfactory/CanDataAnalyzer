52c7b085e2c489cea5a1d9d834f30e310ea69f42
mikanfactory <k952i4j14x17@gmail.com>
2017-02-23 14:39:45 +0900

    [clean] rename command

--- cmd/joinCanAndMovie.py
+++ cmd/joinCanAndMovie.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
         basename = os.path.basename(file)
         name, _ = os.path.splitext(basename)
 
-        tdf = pd.read_table("data/tsv/%s.csv" % name)
+        tdf = pd.read_table("data/tsv/%s.tsv" % name)
         tdf = tdf.ix[1:]
 
         cdf = pd.read_csv("data/csv/%s.csv" % name)
         cdf = cdf.ix[1:]
 
-        df = pd.concat(tdf, cdf, axis=1)
+        df = pd.concat([tdf, cdf], axis=1)
         df.to_csv("data/original/%s.csv" % name, index=False)
